c3a8fb9b7da0363c23106f22a8930a71f1682a50
nobuoka <nobuoka@vividcode.info>
2014-10-21 13:28:28 +0900

    処理の分離

--- app/src/main/java/info/vividcode/android/app/yuyutumblr/MainActivity.java
+++ app/src/main/java/info/vividcode/android/app/yuyutumblr/MainActivity.java
@@ -54,6 +54,17 @@ public class MainActivity extends Activity {
 
     }
 
+    private static class Photo {
+        public final int width;
+        public final int height;
+        public final String url;
+        public Photo(int w, int h, String u) {
+            width = w;
+            height = h;
+            url = u;
+        }
+    }
+
     /**
      * response":[
      *   {
@@ -91,6 +102,38 @@ public class MainActivity extends Activity {
      *   },
      *   {"blog_name":"anisample","id":52300690971,"post_url":"http:\/\/anisample.tumblr.com\/post\/52300690971","slug":"","type":"audio","date":"2013-06-06 14:16:32 GMT","timestamp":1370528192,"state":"published","format":"html","reblog_key":"yIm7xUJf","tags":["\u3086\u3086\u5f0f","\u91ce\u3005\u539f\u3086\u305a\u3053","\u5927\u4e45\u4fdd\u7460\u7f8e"],"short_url":"http:\/\/tmblr.co\/Zj_7osmjNJOR","highlighted":[],"note_count":7,"artist":"\u91ce\u3005\u539f \u3086\u305a\u3053","album":"\u3086\u3086\u5f0f","track_name":"\u3053\u306e\u4e0a\u304c\u3063\u305f\u306e\u3069\u3046\u3057\u3088\u3046","album_art":"http:\/\/25.media.tumblr.com\/tumblr_mnz6bkviyS1s85v8so1_1370528192_cover.jpg","caption":"","player":"\u003Cembed type=\u0022application\/x-shockwave-flash\u0022 src=\u0022http:\/\/assets.tumblr.com\/swf\/audio_player.swf?audio_file=http%3A%2F%2Fwww.tumblr.com%2Faudio_file%2Fanisample%2F52300690971%2Ftumblr_mnz6bkviyS1s85v8s&color=FFFFFF\u0022 height=\u002227\u0022 width=\u0022207\u0022 quality=\u0022best\u0022 wmode=\u0022opaque\u0022\u003E\u003C\/embed\u003E","embed":"\u003Ciframe class=\u0022tumblr_audio_player tumblr_audio_player_52300690971\u0022 src=\u0022http:\/\/anisample.tumblr.com\/post\/52300690971\/audio_player_iframe\/anisample\/tumblr_mnz6bkviyS1s85v8s?audio_file=http%3A%2F
      */
+    private static Photo getAppropriateSizePhotoObject(JSONObject photoInfo) {
+        Photo photo = null;
+        try {
+            JSONArray arr = photoInfo.getJSONArray("alt_sizes");
+            int len = arr.length();
+            for (int i = 0; i < len; ++i) {
+                JSONObject o = arr.getJSONObject(i);
+                int w = o.getInt("width");
+                int h = o.getInt("height");
+                String u = o.getString("url");
+                Photo p = new Photo(w, h, u);
+                if (photo == null) {
+                    photo = p;
+                } else {
+                    if (p.width <= 400) {
+                        if (photo.width < p.width) {
+                            photo = p;
+                        }
+                    } else {
+                        if (p.width < photo.width) {
+                            photo = p;
+                        }
+                    }
+                }
+            }
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return photo;
+    }
+
     private static class PostsAdapter extends ArrayAdapter<JSONObject> {
         private final LayoutInflater inflater;
         private final ImageLoader mImageLoader;
@@ -133,47 +176,6 @@ public class MainActivity extends Activity {
                 Log.d("error", "error", e);
             }
             return convertView;
-        }
-        private static class Photo {
-            public final int width;
-            public final int height;
-            public final String url;
-            public Photo(int w, int h, String u) {
-                width = w;
-                height = h;
-                url = u;
-            }
-        }
-        private Photo getAppropriateSizePhotoObject(JSONObject photoInfo) {
-            Photo photo = null;
-            try {
-                JSONArray arr = photoInfo.getJSONArray("alt_sizes");
-                int len = arr.length();
-                for (int i = 0; i < len; ++i) {
-                    JSONObject o = arr.getJSONObject(i);
-                    int w = o.getInt("width");
-                    int h = o.getInt("height");
-                    String u = o.getString("url");
-                    Photo p = new Photo(w, h, u);
-                    if (photo == null) {
-                        photo = p;
-                    } else {
-                        if (p.width <= 400) {
-                            if (photo.width < p.width) {
-                                photo = p;
-                            }
-                        } else {
-                            if (p.width < photo.width) {
-                                photo = p;
-                            }
-                        }
-                    }
-                }
-            } catch (JSONException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            return photo;
         }
     }
 
