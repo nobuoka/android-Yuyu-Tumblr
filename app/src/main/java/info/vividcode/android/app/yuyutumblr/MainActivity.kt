713a468d80f3da4d07b50d3ff43fe59587bda31f
Nobuoka Yu <nobuoka@vividcode.info>
2018-10-28 14:33:19 +0900

    Rearrange `MainActivity` class

--- app/src/main/java/info/vividcode/android/app/yuyutumblr/MainActivity.kt
+++ app/src/main/java/info/vividcode/android/app/yuyutumblr/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.android.volley.RequestQueue
 import info.vividcode.android.app.yuyutumblr.ui.AndroidMainView
 import info.vividcode.android.app.yuyutumblr.ui.PostAdapter
 import info.vividcode.android.app.yuyutumblr.ui.BitmapCache
@@ -16,28 +19,17 @@ import info.vividcode.android.app.yuyutumblr.web.TumblrWebApi
 
 class MainActivity : AppCompatActivity() {
 
-    private val requestQueueContainer by lazy { RequestQueueLifecycleAwareContainer(this) }
-
-    private val mainApplication: MainApplication by lazy {
-        MainApplication(createMainView(), TumblrWebApi(requestQueueContainer.requestQueue))
-    }
-
-    private fun createMainView(): MainView {
-        // スクロール限界までスクロールしてさらに引っ張ると続きを読み込む仕組み
-        val recyclerView = findViewById<View>(R.id.posts_view) as androidx.recyclerview.widget.RecyclerView
-        val imageLoader = ImageLoader(requestQueueContainer.requestQueue, BitmapCache())
-        val postAdapter = PostAdapter(imageLoader)
-
-        val swipeRefreshLayout = findViewById<View>(R.id.swipe_refresh_layout) as androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-
-        return AndroidMainView.create(this, recyclerView, swipeRefreshLayout, postAdapter)
-    }
+    private lateinit var mainApplication: MainApplication
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val requestQueueContainer = RequestQueueLifecycleAwareContainer(this)
         lifecycle.addObserver(requestQueueContainer)
+
+        val requestQueue = requestQueueContainer.requestQueue
+        mainApplication = MainApplication(createMainView(requestQueue), TumblrWebApi(requestQueue))
         mainApplication.init()
     }
 
@@ -50,6 +42,17 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    private fun createMainView(requestQueue: RequestQueue): MainView {
+        // スクロール限界までスクロールしてさらに引っ張ると続きを読み込む仕組み
+        val recyclerView = findViewById<View>(R.id.posts_view) as RecyclerView
+        val imageLoader = ImageLoader(requestQueue, BitmapCache())
+        val postAdapter = PostAdapter(imageLoader)
+
+        val swipeRefreshLayout = findViewById<View>(R.id.swipe_refresh_layout) as SwipeRefreshLayout
+
+        return AndroidMainView.create(this, recyclerView, swipeRefreshLayout, postAdapter)
     }
 
 }
