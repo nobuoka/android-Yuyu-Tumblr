d8f395943b41e8618834217de0d3981eb4bf1815
Nobuoka Yu <nobuoka@vividcode.info>
2018-10-29 09:04:30 +0900

    Add tests of `MainApplication.getAppropriateSizePhotoObject` method

--- modules/yuyu/usecase/src/test/kotlin/info/vividcode/android/app/yuyutumblr/usecase/MainApplicationTest.kt
+++ modules/yuyu/usecase/src/test/kotlin/info/vividcode/android/app/yuyutumblr/usecase/MainApplicationTest.kt
@@ -5,6 +5,7 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.json.JSONObject
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.lang.RuntimeException
@@ -68,6 +69,66 @@ internal class MainApplicationTest {
             verify(exactly = 1) { mockMainView.stopRefreshingIndicator() }
             verify(exactly = 1) { mockTumblrApi.fetchPosts(any(), any()) }
         }
+    }
+
+    @Nested
+    internal inner class GetAppropriateSizePhotoObjectMethodTest {
+        @Test
+        internal fun normal_targetSizePosition_top() {
+            val json = createTestPhotoJsonWithTargetSizeTop()
+
+            val photo = MainApplication.getAppropriateSizePhotoObject(json)
+
+            val expected = Photo(400, 600, "http://example.com/af8c96/tumblr_mnz8le_400.jpg")
+            Assertions.assertEquals(expected, photo)
+        }
+
+        @Test
+        internal fun normal_targetSizePosition_middle() {
+            val json = createTestPhotoJsonWithTargetSizeMiddle()
+
+            val photo = MainApplication.getAppropriateSizePhotoObject(json)
+
+            val expected = Photo(400, 600, "http://example.com/af8c96/tumblr_mnz8le_400.jpg")
+            Assertions.assertEquals(expected, photo)
+        }
+
+        @Test
+        internal fun invalidJson() {
+            val json = JSONObject("{}")
+
+            val photo = MainApplication.getAppropriateSizePhotoObject(json)
+
+            Assertions.assertNull(photo)
+        }
+    }
+
+    companion object {
+        fun createTestPhotoJsonWithTargetSizeTop() = JSONObject("""
+            {
+              "caption": "",
+              "alt_sizes": [
+                {"width":400,"height":600,"url":"http://example.com/af8c96/tumblr_mnz8le_400.jpg"},
+                {"width":250,"height":375,"url":"http://example.com/af8c96/tumblr_mnz8le_250.jpg"},
+                {"width":100,"height":150,"url":"http://example.com/af8c96/tumblr_mnz8le_100.jpg"},
+                {"width":75,"height":75,"url":"http://example.com/af8c96/tumblr_mnz8le_75.jpg"}
+              ],
+              "original_size": {"width":400,"height":600,"url":"http://example.com/af8c96/tumblr_mnz8le_400.jpg"}
+            }
+        """.trimIndent())
+
+        fun createTestPhotoJsonWithTargetSizeMiddle() = JSONObject("""
+            {
+              "caption": "",
+              "alt_sizes": [
+                {"width":250,"height":375,"url":"http://example.com/af8c96/tumblr_mnz8le_250.jpg"},
+                {"width":100,"height":150,"url":"http://example.com/af8c96/tumblr_mnz8le_100.jpg"},
+                {"width":400,"height":600,"url":"http://example.com/af8c96/tumblr_mnz8le_400.jpg"},
+                {"width":75,"height":75,"url":"http://example.com/af8c96/tumblr_mnz8le_75.jpg"}
+              ],
+              "original_size": {"width":400,"height":600,"url":"http://example.com/af8c96/tumblr_mnz8le_400.jpg"}
+            }
+        """.trimIndent())
     }
 
 }
