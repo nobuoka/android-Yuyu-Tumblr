f43819cbbf812ebc613bf5e78c5593edc93c1899
Nobuoka Yu <nobuoka@vividcode.info>
2018-11-08 03:07:42 +0900

    `PhotoTimeline` class

--- modules/yuyu/ui/src/main/java/info/vividcode/android/app/yuyutumblr/ui/PostAdapter.kt
+++ modules/yuyu/ui/src/main/java/info/vividcode/android/app/yuyutumblr/ui/PostAdapter.kt
@@ -4,37 +4,38 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.toolbox.ImageLoader
 import com.android.volley.toolbox.NetworkImageView
 import info.vividcode.android.app.yuyu.ui.R
 import info.vividcode.android.app.yuyutumblr.usecase.MainApplication
+import info.vividcode.android.app.yuyutumblr.usecase.PhotoTimeline
 import org.json.JSONException
-import org.json.JSONObject
-import java.util.ArrayList
 
-class PostAdapter// Provide a suitable constructor (depends on the kind of dataset)
-(private val mImageLoader: ImageLoader) : androidx.recyclerview.widget.RecyclerView.Adapter<PostAdapter.ViewHolder>() {
-    private val mList: MutableList<JSONObject>
+class PostAdapter(
+        private val mImageLoader: ImageLoader
+) : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
 
-    val lastItem: JSONObject?
-        get() = if (mList.size == 0) null else mList[mList.size - 1]
+    private var photoTimeline: PhotoTimeline? = null
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    class ViewHolder(// each data item is just a string in this case
-            var mView: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(mView)
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
-    init {
-        mList = ArrayList()
+    fun bindPhotoTimeline(photoTimeline: PhotoTimeline) {
+        this.photoTimeline = photoTimeline
+        photoTimeline.addChangeEventListener {
+            when (it) {
+                is PhotoTimeline.ChangeEvent.ItemsAdded -> {
+                    this.notifyItemRangeInserted(it.startPosition, it.itemCount)
+                }
+            } as? Unit?
+        }
     }
 
-    // Create new views (invoked by the layout manager)
-    override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): PostAdapter.ViewHolder {
-        // create a new view
-        val v = LayoutInflater.from(parent.context)
-                .inflate(R.layout.post, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.post, parent, false)
         // set the view's size, margins, paddings and layout parameters...
         return ViewHolder(v)
     }
@@ -44,12 +45,12 @@ class PostAdapter// Provide a suitable constructor (depends on the kind of datas
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
         try {
-            val post = mList[position]
+            val post = photoTimeline?.getPhoto(position) ?: return
             if (post.getString("type") == "photo") {
                 val p = MainApplication.getAppropriateSizePhotoObject(post.getJSONArray("photos").getJSONObject(0))
                 //String url = .getJSONObject("original_size").getString("url");
 
-                val v = holder.mView.findViewById<View>(R.id.image) as NetworkImageView
+                val v = holder.itemView.findViewById<View>(R.id.image) as NetworkImageView
                 v.setImageUrl(p!!.url, mImageLoader)
                 v.minimumHeight = p.height
                 v.minimumWidth = 36
@@ -71,12 +72,7 @@ class PostAdapter// Provide a suitable constructor (depends on the kind of datas
 
     // Return the size of your dataset (invoked by the layout manager)
     override fun getItemCount(): Int {
-        return mList.size
-    }
-
-    fun add(items: List<JSONObject>) {
-        mList.addAll(items)
-        notifyDataSetChanged()
+        return photoTimeline?.size ?: 0
     }
 
 }
