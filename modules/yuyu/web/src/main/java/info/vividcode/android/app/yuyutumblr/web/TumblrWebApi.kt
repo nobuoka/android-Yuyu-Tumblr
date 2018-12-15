6018e26c47cfabb861fcfa552cfe00411b72aecc
Nobuoka Yu <nobuoka@vividcode.info>
2018-12-15 19:38:07 +0900

    Add OkHttp to dependencies of web module and remove Volley from it

--- modules/yuyu/web/src/main/java/info/vividcode/android/app/yuyutumblr/web/TumblrWebApi.kt
+++ modules/yuyu/web/src/main/java/info/vividcode/android/app/yuyutumblr/web/TumblrWebApi.kt
@@ -1,40 +1,52 @@
 package info.vividcode.android.app.yuyutumblr.web
 
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.toolbox.JsonObjectRequest
 import info.vividcode.android.app.yuyutumblr.usecase.TumblrApi
 import info.vividcode.android.app.yuyutumblr.usecase.TumblrPost
-import org.json.JSONException
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.Request
+import org.json.JSONObject
+import java.io.IOException
 
-class TumblrWebApi(private val requestQueue: RequestQueue) : TumblrApi {
+class TumblrWebApi(private val callFactory: Call.Factory, private val callbackExecutor: CallbackExecutor) : TumblrApi {
+
+    interface CallbackExecutor {
+        fun <T> execute(value: T, execution: (T) -> Unit)
+    }
 
     override fun fetchPosts(lastTimestamp: Int?, callback: (TumblrApi.Result<List<TumblrPost>>) -> Unit) {
         // API キーは Tumblr のドキュメントにのってたやつ。 ほんとは各自取得する必要がある?
         // http://www.tumblr.com/docs/en/api/v2#tagged-method
         var uri = "https://api.tumblr.com/v2/tagged?tag=%E3%82%86%E3%82%86%E5%BC%8F" + "&api_key=fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4"
         if (lastTimestamp != null) {
-            try {
-                uri += "&before=$lastTimestamp"
-            } catch (e: JSONException) {
-                // TODO Auto-generated catch block
-                e.printStackTrace()
-            }
+            uri += "&before=$lastTimestamp"
         }
 
-        // リクエスト生成
-        val req = JsonObjectRequest(uri, null, Response.Listener { response ->
-            val responseContent = try {
-                TumblrApi.Result.Success(TumblrResponseConverter.convertTumblrPostsResponse(response))
-            } catch (e: Throwable) {
-                TumblrApi.Result.Failure<List<TumblrPost>>(e)
+        val request = Request.Builder()
+                .url(uri)
+                .build()
+        callFactory.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call, response: okhttp3.Response) {
+                val responseContent: TumblrApi.Result<List<TumblrPost>> = try {
+                    if (response.isSuccessful) {
+                        response.body()?.let {
+                            val jsonObject = it.string().let(::JSONObject)
+                            TumblrApi.Result.Success(TumblrResponseConverter.convertTumblrPostsResponse(jsonObject))
+                        } ?: TumblrApi.Result.Failure(ErrorResponseException("Not expected response body", response))
+                    } else {
+                        TumblrApi.Result.Failure(ErrorResponseException("Not successful response", response))
+                    }
+                } catch (e: Throwable) {
+                    TumblrApi.Result.Failure(e)
+                }
+                callbackExecutor.execute(responseContent, callback)
             }
-            callback(responseContent)
-        }, Response.ErrorListener { error ->
-            callback(TumblrApi.Result.Failure(error))
+            override fun onFailure(call: Call, e: IOException) {
+                callbackExecutor.execute(TumblrApi.Result.Failure(e), callback)
+            }
         })
-        // リクエストをキューに追加
-        requestQueue.add(req)
     }
+
+    class ErrorResponseException(message: String, val response: okhttp3.Response) : RuntimeException(message)
 
 }
